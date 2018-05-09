8d0d8b6ae46e987208706bf13d9e8e408d60fad3
Elliot Pryde <EPryde@airpoint.net>
2018-05-09 15:31:56 +0100

    get-admin-password script working on hyper-v machine

--- jenkins/master/scripts/get-admin-password.sh
+++ jenkins/master/scripts/get-admin-password.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-password=$(docker exec jenkins-master cat /var/jenkins_home/secrets/initialAdminPassword)
+password=$(docker exec jenkins-master cat //var/jenkins_home/secrets/initialAdminPassword)
 
 echo $password
