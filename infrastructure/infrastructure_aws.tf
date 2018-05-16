3822db900f6ed6480f8218d342ac6b37f85c120a
Elliot Pryde <EPryde@airpoint.net>
2018-05-16 22:33:31 +0100

    Defines ssh key-pair and assigns it to the ec2 t2.nano instance

--- infrastructure/infrastructure_aws.tf
+++ infrastructure/infrastructure_aws.tf
@@ -21,8 +21,14 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+resource "aws_key_pair" "ssh-key-pair" {
+  key_name   = "ssh-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNfVl+XOfCXd1qAs1ZAOIRUuofz8KURoYJJK3g7/YJ660WWxcCRS73PMlcPsjbPunIi/maKDpuv3U2/rzknZGfKcUYsAtnPm7LD70zEZknTZtb4vM2+xtV5QLoFvB9MLoZr66WlJr5a4VQlyOa39mA5eo0RuaBtRAnCZrnA87AGGNv1CbCq5lhFO9QrFP++1Rd6Eqh+4XoIr1HL6N2BUA4QNKM2/hDgjtH+4gCOdeF1PJYvWf921mvsQfHGWLuD1Ldnb/xYKc/657Cg3z3lZKfmyCBwJ74QMamfoEwjJdtNa15/5VwLYqTnx904hnKZW2DpmuqjsQ8Y06P1HJlP9UP"
+}
+
 resource "aws_instance" "example_ubuntu_ec2_t2_nano" {
   ami             = "ami-f4f21593"
   instance_type   = "t2.nano"
   security_groups = ["allow_all"]
+  key_name        = "ssh-key"
 }
