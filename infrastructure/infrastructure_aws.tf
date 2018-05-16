ec79070227eb7c3c292271560f08301b004e24ec
Elliot Pryde <EPryde@airpoint.net>
2018-05-17 00:11:38 +0100

    Launches ec2 instance using latest jenkins_master tagged AMI

--- infrastructure/infrastructure_aws.tf
+++ infrastructure/infrastructure_aws.tf
@@ -26,8 +26,25 @@ resource "aws_key_pair" "ssh-key-pair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNfVl+XOfCXd1qAs1ZAOIRUuofz8KURoYJJK3g7/YJ660WWxcCRS73PMlcPsjbPunIi/maKDpuv3U2/rzknZGfKcUYsAtnPm7LD70zEZknTZtb4vM2+xtV5QLoFvB9MLoZr66WlJr5a4VQlyOa39mA5eo0RuaBtRAnCZrnA87AGGNv1CbCq5lhFO9QrFP++1Rd6Eqh+4XoIr1HL6N2BUA4QNKM2/hDgjtH+4gCOdeF1PJYvWf921mvsQfHGWLuD1Ldnb/xYKc/657Cg3z3lZKfmyCBwJ74QMamfoEwjJdtNa15/5VwLYqTnx904hnKZW2DpmuqjsQ8Y06P1HJlP9UP"
 }
 
-resource "aws_instance" "example_ubuntu_ec2_t2_nano" {
-  ami             = "ami-f4f21593"
+# Find my latest available jenkins_master AMI
+data "aws_ami" "jenkins_master" {
+  owners = ["670785527082"] # Canonical
+
+  most_recent = true
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "tag:Component"
+    values = ["jenkins_master"]
+  }
+}
+
+resource "aws_instance" "jenkins_master_vm" {
+  ami             = "${data.aws_ami.jenkins_master.id}"
   instance_type   = "t2.nano"
   security_groups = ["allow_all"]
   key_name        = "ssh-key"
