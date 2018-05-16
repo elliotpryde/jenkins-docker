d18951312857b3b8b5a65fe7459145bfc853d028
Elliot Pryde <EPryde@airpoint.net>
2018-05-16 21:50:54 +0100

    Adds allow_all security group to AWS ec2 t2.nano instance

--- infrastructure/infrastructure_aws.tf
+++ infrastructure/infrastructure_aws.tf
@@ -2,7 +2,27 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example_ubuntu_ec2_t2_nano" {
-  ami           = "ami-f4f21593"
-  instance_type = "t2.nano"
+  ami             = "ami-f4f21593"
+  instance_type   = "t2.nano"
+  security_groups = ["allow_all"]
 }
