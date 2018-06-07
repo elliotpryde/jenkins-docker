bcc0094107f3231ae4824f536b65b5115f746ace
Elliot Pryde <EPryde@airpoint.net>
2018-06-07 23:09:19 +0100

    Adds template to initialise a docker cloud via groovy init script

---
+++ docker/jenkins-master/config/configure-docker-cloud.groovy
@@ -0,0 +1,98 @@
+import com.nirima.jenkins.plugins.docker.DockerCloud
+import com.nirima.jenkins.plugins.docker.DockerTemplate
+import com.nirima.jenkins.plugins.docker.DockerTemplateBase
+import com.nirima.jenkins.plugins.docker.launcher.AttachedDockerComputerLauncher
+import io.jenkins.docker.connector.DockerComputerAttachConnector
+import jenkins.model.Jenkins
+
+// parameters
+def dockerTemplateBaseParameters = [
+  bindAllPorts:       false,
+  bindPorts:          '',
+  cpuShares:          null,
+  dnsString:          '',
+  dockerCommand:      '',
+  environmentsString: '',
+  extraHostsString:   '',
+  hostname:           '',
+  image:              'jenkinsci/slave:latest',
+  macAddress:         '',
+  memoryLimit:        null,
+  memorySwap:         null,
+  network:            '',
+  privileged:         false,
+  pullCredentialsId:  '',
+  tty:                true,
+  volumesFromString:  '',
+  volumesString:      ''
+]
+
+def DockerTemplateParameters = [
+  instanceCapStr: '',
+  labelString:    'docker.local.jenkins.slave',
+  remoteFs:       '/home/jenkins'
+]
+
+def dockerCloudParameters = [
+  connectTimeout:   5,
+  containerCapStr:  '100',
+  credentialsId:    '',
+  dockerHostname:   '',
+  name:             'AWS-docker-slave-host',
+  readTimeout:      15,
+  serverUrl:        'tcp://ec2-18-130-100-214.eu-west-2.compute.amazonaws.com:2376', // epTODO use jenkins slave host DNS
+  version:          ''
+]
+
+// https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java
+DockerTemplateBase dockerTemplateBase = new DockerTemplateBase(
+  dockerTemplateBaseParameters.image,
+  dockerTemplateBaseParameters.pullCredentialsId,
+  dockerTemplateBaseParameters.dnsString,
+  dockerTemplateBaseParameters.network,
+  dockerTemplateBaseParameters.dockerCommand,
+  dockerTemplateBaseParameters.volumesString,
+  dockerTemplateBaseParameters.volumesFromString,
+  dockerTemplateBaseParameters.environmentsString,
+  dockerTemplateBaseParameters.hostname,
+  dockerTemplateBaseParameters.memoryLimit,
+  dockerTemplateBaseParameters.memorySwap,
+  dockerTemplateBaseParameters.cpuShares,
+  dockerTemplateBaseParameters.bindPorts,
+  dockerTemplateBaseParameters.bindAllPorts,
+  dockerTemplateBaseParameters.privileged,
+  dockerTemplateBaseParameters.tty,
+  dockerTemplateBaseParameters.macAddress,
+  dockerTemplateBaseParameters.extraHostsString
+)
+
+// https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplate.java
+DockerTemplate dockerTemplate = new DockerTemplate(
+  dockerTemplateBase,
+  new DockerComputerAttachConnector(), //epTODO change this to SSH connector
+  DockerTemplateParameters.labelString,
+  DockerTemplateParameters.remoteFs,
+  DockerTemplateParameters.instanceCapStr
+)
+
+// https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerCloud.java
+DockerCloud dockerCloud = new DockerCloud(
+  dockerCloudParameters.name,
+  [dockerTemplate],
+  dockerCloudParameters.serverUrl,
+  dockerCloudParameters.containerCapStr,
+  dockerCloudParameters.connectTimeout,
+  dockerCloudParameters.readTimeout,
+  dockerCloudParameters.credentialsId,
+  dockerCloudParameters.version,
+  dockerCloudParameters.dockerHostname
+)
+
+// get Jenkins instance
+Jenkins jenkins = Jenkins.getInstance()
+
+// add cloud configuration to Jenkins
+jenkins.clouds.add(dockerCloud)
+
+// save current Jenkins state to disk
+jenkins.save()
