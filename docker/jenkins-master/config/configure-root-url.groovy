73e9205837b9a76d69f2556f1e2322da08785df6
Elliot Pryde <EPryde@airpoint.net>
2018-06-17 16:57:59 +0100

    Refactors configure-root-url groovy script

--- docker/jenkins-master/config/configure-root-url.groovy
+++ docker/jenkins-master/config/configure-root-url.groovy
@@ -3,5 +3,5 @@ import jenkins.model.JenkinsLocationConfiguration
 def env = System.getenv()
 
 jlc = JenkinsLocationConfiguration.get()
-jlc.setUrl(System.getenv()["JENKINS_ROOT"])
+jlc.setUrl(env.JENKINS_ROOT)
 jlc.save()
