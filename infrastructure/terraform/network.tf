92b7209eff34953419d4a0419a2a9fcf9435c6eb
Elliot Pryde <EPryde@airpoint.net>
2018-06-17 17:00:48 +0100

    Adds more specific security groups

--- infrastructure/terraform/network.tf
+++ infrastructure/terraform/network.tf
@@ -1,13 +1,54 @@
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh connections on port 22"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "allow_docker_api" {
+  name        = "allow_docker_api"
+  description = "Allow Docker API connections on port 2376"
+
+  ingress {
+    from_port   = 2376
+    to_port     = 2376
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP connections on port 80"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPS connections on port 443"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_all_outbound" {
+  name        = "allow_all_outbound"
+  description = "Allow all egress connections"
 
   egress {
     from_port   = 0
