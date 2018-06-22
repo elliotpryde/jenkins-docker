eb5903919db8f994f1164766f81650995e005912
Elliot Pryde <EPryde@airpoint.net>
2018-06-22 15:02:19 +0100

    Provisioning linux docker slave VM
    Using ansible to install docker when building the VM image, and exposing
    the docker remote TCP API on port 2376 by editing the daemon.json

--- infrastructure/terraform/dns.tf
+++ infrastructure/terraform/dns.tf
@@ -1,15 +1,20 @@
-resource "aws_eip" "jenkins_master_eip" {
-  instance = "${aws_instance.jenkins_master_vm.id}"
+# resource "aws_eip" "jenkins_master_eip" {
+#   instance = "${aws_instance.jenkins_master_vm.id}"
+# }
+
+resource "aws_eip" "linux_docker_slave_host_eip" {
+  instance = "${aws_instance.linux_docker_slave_host.id}"
 }
 
 resource "aws_route53_zone" "elliotpryde" {
   name = "elliotpryde.com"
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.elliotpryde.zone_id}"
-  name    = "jenkins.elliotpryde.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_eip.jenkins_master_eip.public_ip}"]
-}
+# resource "aws_route53_record" "www" {
+#   zone_id = "${aws_route53_zone.elliotpryde.zone_id}"
+#   name    = "jenkins.elliotpryde.com"
+#   type    = "A"
+#   ttl     = "300"
+#   records = ["${aws_eip.jenkins_master_eip.public_ip}"]
+# }
+
