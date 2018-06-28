24d6d8565329ef2fbb04efb3e26639f6ed160fad
Elliot Pryde <elliotpryde@gmail.com>
2018-06-28 23:26:05 +0100

    Adds an example google compute instance

--- infrastructure/terraform/vm.tf
+++ infrastructure/terraform/vm.tf
@@ -43,3 +43,23 @@ resource "aws_instance" "linux_docker_slave_host" {
   security_groups = ["allow_ssh", "allow_docker_api", "allow_all_outbound"]
   key_name        = "elliot_ssh"
 }
+
+resource "google_compute_instance" "default" {
+  name         = "example"
+  machine_type = "f1-micro"
+  zone         = "europe-west2-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Having this block creates an ephemeral IP which is accessible from the internet
+    }
+  }
+}
