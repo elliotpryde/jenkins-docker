e99d74a36347ec049050cec673e5ef03aee90e89
Elliot Pryde <elliotpryde@gmail.com>
2018-07-16 20:38:48 +0100

    20 Adds both declarative and scripted pipeline jobs

---
+++ docker/jenkins-master/config/configure-scripted-pipeline-job.groovy
@@ -0,0 +1,29 @@
+import jenkins.model.Jenkins
+import hudson.plugins.git.*
+import hudson.triggers.SCMTrigger
+
+def repositoryLocation = "https://elliotpryde@bitbucket.org/elliotpryde/jenkins-docker-pipeline.git"
+def jenkinsfileName = "Jenkinsfile-scripted"
+def branchSpec = "scripted-pipeline-docker-slave"
+def cronTimer = "H/2 * * * *"
+def ignorePostCommitHooks = false
+def jobName = "Scripted pipeline job"
+
+def scm = new GitSCM(repositoryLocation)
+scm.branches = [new BranchSpec(branchSpec)]
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, jenkinsfileName)
+def inputTriggers = [new SCMTrigger(cronTimer, ignorePostCommitHooks)]
+
+if(!Jenkins.instance.isQuietingDown()) {
+    def j = Jenkins.instance
+    def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(j, jobName)
+    job.definition = flowDefinition
+    job.triggers = inputTriggers
+
+    j.reload()
+    println "Successfully initialised SCM pipeline job ${repositoryLocation} - ${jenkinsfileName}."
+}
+else {
+    println "Shutdown mode enabled. Initialisation of SCM pipeline job ${repositoryLocation} - ${jenkinsfileName} SKIPPED."
+}
