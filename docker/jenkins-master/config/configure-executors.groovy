fea1ba731e8ba456cbcb04054700e8f567b1b921
Elliot Pryde <EPryde@airpoint.net>
2018-07-16 13:41:46 +0100

    Temporarily enables a single executor on the master

--- docker/jenkins-master/config/configure-executors.groovy
+++ docker/jenkins-master/config/configure-executors.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 
 if(!Jenkins.instance.isQuietingDown()) {
-    Jenkins.instance.setNumExecutors(0)
+    Jenkins.instance.setNumExecutors(1)
     println "Executors on master DISABLED successfully."
 }
 else {
